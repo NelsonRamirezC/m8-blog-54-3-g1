4771aac2a5c95427dc47f8d87409bf146832bb34
Nelson Ramírez <xhyskoax@gmail.com>
2026-08-26 12:33:24 -0400

    se crea middleware de validación de body para rutas post y put y se crea endpoint con su controlador para crear usuarios, se agrega lógica where para filtar usuarios que no sean admin en controlador getAllUsuarios

--- src/controllers/usuarios.controller.js
+++ src/controllers/usuarios.controller.js
@@ -1,13 +1,62 @@
 import Usuario from "../models/Usuario.model.js";
+import sequelize from "../config/db.js";
 
 export const getAllUsuarios = async (req, res) => {
     try {
-    
-        const usuarios = await Usuario.findAll();
-        
-        res.json({status: "ok", usuarios});
+        const usuarios = await Usuario.findAll({
+            attributes: ["id", "nombre", "email"],
+            // where: {
+            //     admin: false
+            // }
+        });
+
+        res.json({ status: "ok", usuarios });
     } catch (error) {
         console.log(error);
-        res.status(500).json({ message: "Error interno del servidor"});
+        res.status(500).json({ message: "Error interno del servidor" });
     }
-}
+};
+
+export const createUsuario = async (req, res) => {
+
+    const t = await sequelize.transaction();
+    try {
+        let { nombre, email, password } = req.body;
+
+        if (!nombre || !email || !password) {
+            await t.rollback();
+            return res
+                .status(400)
+                .json({
+                    status: "fail",
+                    message: `No se proporcionan los campos requeridos: [nombre, email, password].`,
+                });
+        }
+
+        const usuario = await Usuario.findOne({
+            where: {
+                email,
+            },
+            transaction: t
+        });
+
+        if (usuario) {
+            await t.rollback();
+            return res
+                .status(400)
+                .json({
+                    status: "fail",
+                    message: `Ya existe un usuario registrado con el email: ${email}, si usted es el propietario de dicho email, intente recuperar su password, de lo contrario debe ponerse en contacto contacto con el administrador: admin@admin.com`,
+                });
+        }
+
+        const newUsuario = await Usuario.create({ nombre, email, password }, {transaction: t});
+
+        await t.commit();
+        res.json({ status: "ok", usuario: newUsuario });
+    } catch (error) {
+        await t.rollback();
+        console.log(error);
+        res.status(500).json({ message: "Error interno del servidor" });
+    }
+};
--- src/routes/usuarios.routes.js
+++ src/routes/usuarios.routes.js
@@ -1,10 +1,15 @@
 import express from "express";
 import * as usuariosController from "../controllers/usuarios.controller.js";
+import validateBody from "../middlewares/validateBody.js";
+
 
 const usuariosRoutes = express.Router();
 
 //OBTENER TODOS LOS USUARIOS
 usuariosRoutes.get("/", usuariosController.getAllUsuarios);
+
+//REGISTRAR NUEVOS USUARIOS
+usuariosRoutes.post("/", validateBody, usuariosController.createUsuario);
 
 
 export default usuariosRoutes;
