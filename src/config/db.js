94c0c46b311197059e7d084ea780de30e6f11d95
Nelson Ramírez <xhyskoax@gmail.com>
2026-09-03 12:20:04 -0400

    Se actulizan archivos .env y se usan las variables de entorno para proteger la URI de conección con la base de datos

--- src/config/db.js
+++ src/config/db.js
@@ -1,6 +1,6 @@
 import { Sequelize } from 'sequelize';
 
-const URI_DATABASE = "postgres://postgres:123456@localhost:5432/m8_blog_54_3_g1";
+const URI_DATABASE = process.env.URI_DATABASE;
 
 const sequelize = new Sequelize(URI_DATABASE, {
     quoteIdentifiers: false
